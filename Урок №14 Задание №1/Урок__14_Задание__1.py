0be398ba56d3744c7929afd1eb478e1646de2be1
Администратор <@DESKTOP-9LVUU29>
2025-03-09 05:34:48 +0300

    Добавьте файлы проекта.

---
+++ Урок №14 Задание №1/Урок__14_Задание__1.py
@@ -0,0 +1,13 @@
+﻿def print_list_recursive(my_list, index=0):
+    # Проверяем, достигли ли конца списка
+    if index < len(my_list):
+        print(my_list[index])  # Выводим текущий элемент
+        print_list_recursive(my_list, index + 1)  # Рекурсивный вызов для следующего элемента
+    else:
+        print("Конец списка")  # Сообщение о завершении
+
+# Исходный список
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+# Вызов функции
+print_list_recursive(my_list)
